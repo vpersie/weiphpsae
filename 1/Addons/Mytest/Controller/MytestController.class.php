b544e688af91cf510a75161de5d494392b746e8d
1435635181@qq.com <1435635181@qq.com@906a03fd-6742-4de3-9af4-27e913f16abb>
2014-12-15 03:05:52 +0000

    我的test插件

---
+++ 1/Addons/Mytest/Controller/MytestController.class.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Addons\Mytest\Controller;
+use Home\Controller\AddonsController;
+
+class MytestController extends AddonsController{
+
+}
---
+++ 1/Addons/Mytest/Model/MytestModel.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Addons\Mytest\Model;
+use Think\Model;
+
+/**
+ * Mytest模型
+ */
+class MytestModel extends Model{
+
+}
