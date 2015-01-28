e556ef084f8ef73029f2ace590f8fe429bb33abd
1435635181@qq.com <1435635181@qq.com@906a03fd-6742-4de3-9af4-27e913f16abb>
2015-01-28 08:43:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/Addons/Idioms/Model/WeixinAddonModel.class.php

--- 1/Addons/Idioms/Model/WeixinAddonModel.class.php
+++ 1/Addons/Idioms/Model/WeixinAddonModel.class.php
@@ -13,7 +13,7 @@ class WeixinAddonModel extends WeixinModel{
         $api = 'http://i.itpk.cn/api.php?question=@cy';
         if($dataArr['Content']=='成语接龙' || $dataArr['Content']=='Idioms'){
         	$keywordArr['step'] = 'input';
-        	set_user_status('Idioms', $keywordArr);
+        	myset_user_status('Idioms', $keywordArr);
         	$this->replyText('请输入一个成语，比如：无疾而终');
         }
         if($keywordArr['step']=='input'){
@@ -24,18 +24,25 @@ class WeixinAddonModel extends WeixinModel{
         $reply = file_get_contents($api.$dataArr['Content']);
         if($reply=='别来骗人家，不算随便打4个字就是成语哒！') || $reply=='成语必须为4个汉字'){
 	       $keywordArr['step'] = 'input';
-	       set_user_status('Idioms' , $keywordArr);
+	       myset_user_status('Idioms' , $keywordArr);
 	       $this->replyText($reply."\n".'重新输入一个成语开始继而，输入【退出】退出成语接龙');
 
         }else{
         	$keywordArr['step'] = 'input';
-        	set_user_status('Idioms' , $keywordArr);
+        	myset_user_status('Idioms' , $keywordArr);
         	$this->replyText($reply);
         }
 	} 
        
 }
 
+    
+    public function myset_user_status() {
+		$user_status['addon'] = $addon;
+	    $user_status['keywordArr'] = $keywordArr;
+	    $openid = get_openid();
+	    return S('user_status_'.$openid, $user_status);
+	}
 	// 关注公众号事件
 	public function subscribe() {
 		return true;
