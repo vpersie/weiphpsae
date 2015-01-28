dfa509f643e384bc726f9685f99bf0dc83cf5f9f
1435635181@qq.com <1435635181@qq.com@906a03fd-6742-4de3-9af4-27e913f16abb>
2015-01-28 08:34:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/Addons/Idioms/Model/WeixinAddonModel.class.php

--- 1/Addons/Idioms/Model/WeixinAddonModel.class.php
+++ 1/Addons/Idioms/Model/WeixinAddonModel.class.php
@@ -10,30 +10,29 @@ class WeixinAddonModel extends WeixinModel{
 	function reply($dataArr, $keywordArr = array()) {
 		$config = getAddonConfig ( 'Idioms' ); // 获取后台插件的配置参数	
 		//dump($config);
-        /*$api = 'http://i.itpk.cn/api.php?question=@cy';
+        $api = 'http://i.itpk.cn/api.php?question=@cy';
         if($dataArr['Content']=='成语接龙' || $dataArr['Content']=='Idioms'){
         	$keywordArr['step'] = 'input';
         	set_user_status('Idioms', $keywordArr);
-        	$this->reply('请输入一个成语，比如：无疾而终');
+        	$this->replyText('请输入一个成语，比如：无疾而终');
         }
         if($keywordArr['step']=='input'){
         	if($dataArr['Content'] == '退出'){
-        	$this->reply('你已退出成语接龙模式，再次回复【成语接龙】即可进入。。');
+        	$this->replyText('你已退出成语接龙模式，再次回复【成语接龙】即可进入。。');
         	return false;
         }
         $reply = file_get_contents($api.$dataArr['Content']);
         if($reply=='别来骗人家，不算随便打4个字就是成语哒！') || $reply=='成语必须为4个汉字'){
 	       $keywordArr['step'] = 'input';
 	       set_user_status('Idioms' , $keywordArr);
-	       $this->reply($reply."\n".'重新输入一个成语开始继而，输入【退出】退出成语接龙');
+	       $this->replyText($reply."\n".'重新输入一个成语开始继而，输入【退出】退出成语接龙');
 
         }else{
         	$keywordArr['step'] = 'input';
         	set_user_status('Idioms' , $keywordArr);
         	$this->replyText($reply);
         }
-	} */
-        $this->replyText('你已退出成语接龙模式，再次回复【成语接龙】即可进入。。');
+	} 
        
 }
 
